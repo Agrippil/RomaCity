0b1d7edbcdb09c61645da2d1eb56242eb9db5b04
Agrippil <Agrippil@yandex.ua>
2014-08-05 15:11:15 +0300

    hhhhh

--- src/PGTRent.php
+++ src/PGTRent.php
@@ -1,19 +1,19 @@
 <?php
-namespace roma/city;
+namespace Roman/Town;
 
 $PGT = $_GET['PGT'];
 $YearBuilt = $_GET['YearBuilt'];
 $latitude = $_GET['latitude'];
 $longitude = $_GET['longitude'];
 $Homes = $_GET['Homes'];
-$Plot = $_GET['Plot']; 
+$Plot = $_GET['Plot'];
 //Все функции которые будут указаны здесь после отрозятся в окне браузера.
     $PGTRent = new PGTRent($PGT, $YearBuilt, $latitude, $longitude, $Homes, $Plot);
     echo 'Площадь ПГТ: ' . $PGTRent->PGTPlot().'<br>';
     echo 'Численность населения: ' . $PGTRent->Peoples().'<br>';
     echo 'Суммарный налог за землю: ' . $PGTRent->Mzda().'<br>';
     echo 'Размер сбора с каждого дома: ' . $PGTRent->Nalog().'<br>';
-   	
+
 	class PGTRent {
 	public $PGT;
 	public $YearBuilt;
@@ -21,7 +21,7 @@ $Plot = $_GET['Plot'];
 	public $longitude;
 	public $Homes;
 	public $Plot;
-	
+
    /* const apartment = 4; На каждом этоже 4 квартиры */
     //const PlotHome = 1; //площадь одного участка 1 гт.;
     const PayPlot= 120; //120 грн. - оплата за 1 гтр. земли;
@@ -29,12 +29,12 @@ $Plot = $_GET['Plot'];
     const PlotKlub = 1.2; /*1,2 гт.- площадь клуба */
 	const People = 4; /*4 - среднее количество людей живущих в одном доме. */
 	const Garbage = 5; /*5 грн. - налог с одного человека за вывоз мусора*/
-// - рассчитывает бюджет населенного пункта в зависимости от размера налога на землю, 
+// - рассчитывает бюджет населенного пункта в зависимости от размера налога на землю,
 //полученного со всех домов;
 //- рассчитывает количество населения, проживающего в населенном пункте;
 // - выводит информацию о населенном пункте.
 	/* ',' - Sапятые нельзя использовать в именах переменных. */
-    
+
 	public function __construct($PGT, $YearBuilt, $latitude, $longitude, $Homes, $Plot){
 	        $this->PGT = $PGT;
 	        $this->YearBuilt = $YearBuilt;
@@ -43,31 +43,31 @@ $Plot = $_GET['Plot'];
             $this->Homes = $Homes;
 			$this->Plot = $Plot;
             echo "Данные района: Название поселка - $PGT," .'<br>';
-            echo         		    "Год основания - $YearBuilt год.".'<br>';     
-			echo 	 "Координаты:   Широта - $latitude*,    Долгота - $longitude*." .'<br>';  
-            echo                   "Домов - $Homes шт.".'<br>'; 
-            echo                    "Площадь участков - $Plot гт.".'<br>';   
-        }        
+            echo         		    "Год основания - $YearBuilt год.".'<br>';
+			echo 	 "Координаты:   Широта - $latitude*,    Долгота - $longitude*." .'<br>';
+            echo                   "Домов - $Homes шт.".'<br>';
+            echo                    "Площадь участков - $Plot гт.".'<br>';
+        }
        /* function apartments() { *Расчет количества квартир в доме *
 		 $apartments = $this->floors * $this->tranc * self::apartment;
-            return $apartments;   
-        } */  
+            return $apartments;
+        } */
         function PGTPlot(){  /*Расчет площади ПГТ */
             $PGTPlot=$this->Homes*$this->Plot + self::PlotShop + self::PlotKlub;
             return $PGTPlot;
-        } 
+        }
         function Peoples(){  /*Расчет населения ПГТ */
             $Peoples=$this->Homes*self::People;
             return $Peoples;
-        } 
+        }
         function Mzda(){  /*Расчет суммарного налога */
             $Mzda=$this->PGTPlot() * self::PayPlot;
             return $Mzda;
-        } 
-		
+        }
+
 		function Nalog(){  //Размер оплаты с каждого дома
 		$Nalog=$this->Mzda()/$this->Homes;
             return  $Nalog;
-        } 
+        }
     }
 ?>
